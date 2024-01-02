4a8256c262a2b6e45211200aba64de1e94dcad77
juniorbueno0 <jjunior.buen00@gmail.com>
2024-01-02 14:20:05 -0700

    lsp working!

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,6 @@
 return {
   -- plugin
-  { "ellisonleao/gruvbox.nvim" },
+   { "ellisonleao/gruvbox.nvim" },
   -- plugin config
   {
     "LazyVim/LazyVim",
@@ -10,6 +10,56 @@ return {
   },
   -- more plugins
   { "catppuccin/nvim", name = "catppuccin"},
+  -- notify
+  { "rcarriga/nvim-notify" },
     
-  { 'dundalek/lazy-lsp.nvim', requires = { 'neovim/nvim-lspconfig' } }
+    
+    
+  -- safe
+    -- NVIM DOCS
+  { "folke/neodev.nvim", opts = {} },
+  { "loichyan/nerdfix" },
+    
+  { "RRethy/vim-illuminate" }, --highlight word concurrences     
+    
+    -- lsp
+  { 'dundalek/lazy-lsp.nvim', requires = { 'neovim/nvim-lspconfig' } },
+    
+    -- SCROLLING --
+  --{ "lewis6991/satellite.nvim" }, -- need version >nvim 0.10
+  --{ "karb94/neoscroll.nvim" },
+    
+    -- snippets
+  { "rafamadriz/friendly-snippets" },
+  
+  { 
+	"L3MON4D3/LuaSnip",
+	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!).
+    build = "make install_jsregexp"
+  },
+    
+    
+    
+    -- cmp plugins
+  "hrsh7th/nvim-cmp", -- The completion plugin
+    
+  "hrsh7th/cmp-buffer", -- buffer completions
+  "hrsh7th/cmp-path", -- path completions
+  "hrsh7th/cmp-cmdline", -- cmdline completions
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lua", -- lua nvim scripting
+  "saadparwaiz1/cmp_luasnip", -- snippet completions
+  "windwp/nvim-autopairs",
+
+    -- lsp 
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig", -- enable LSP
+  "williamboman/nvim-lsp-installer",
+  "ray-x/lsp_signature.nvim",
+  "jose-elias-alvarez/null-ls.nvim",
+  "antoinemadec/FixCursorHold.nvim", -- This is needed to fix lsp doc highlight
+    
+    
 }
